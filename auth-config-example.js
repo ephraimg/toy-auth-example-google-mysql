40c14bf32e034493f5e2c4f8a90521d51da220f7
Ephraim Glick <ephraim.glick@gmail.com>
2017-11-24 23:28:12 -0800

    Fix auth config example

--- auth-config-example.js
+++ auth-config-example.js
@@ -1,2 +1,5 @@
-const GOOGLE_CLIENT_ID = 'the-google-id-of-your-app--get-one-from-google';
-const GOOGLE_CLIENT_SECRET = 'your-secret-key--get-one-from-google';
+module.exports.GOOGLE_CLIENT_ID = 'the-google-id-of-your-app--get-one-from-google';
+module.exports.GOOGLE_CLIENT_SECRET = 'your-secret-key--get-one-from-google';
+module.exports.MYSQL_USER = 'your-username-for-mysql';
+module.exports.MYSQL_PASSWORD = 'your-password-for-mysql';
+module.exports.SESSION_SECRET = 'your-choice-of-secret-for-sessions';
